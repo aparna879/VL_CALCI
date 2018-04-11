22ba2c554dabf0d4ccf1346bc394c64305ddf8c9
aparna879 <aparna879@yahoo.in>
2018-04-11 17:52:55 +0530

    All Basic functions of Calculator

--- app/src/main/java/com/example/aparna879/vl_calci/MainActivity.java
+++ app/src/main/java/com/example/aparna879/vl_calci/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.aparna879.vl_calci;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    String current_value="0";
+    String current_value="0",temp="0",dis="0",c;
     boolean status;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private void setvalue(String n){
+        EditText e = findViewById(R.id.show);
+        e.setText(n);
+    }
+
     public void numpad(View view){
         String s=(String)view.getTag();
         if(s==".")
@@ -49,4 +56,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void operation(View view){
+         temp = current_value;
+         current_value="0";
+         final Button badd = (Button) findViewById(R.id.add);
+         final Button bsub = (Button) findViewById(R.id.sub);
+         final Button bdivide = (Button) findViewById(R.id.divide);
+         final Button bx = (Button) findViewById(R.id.x);
+
+         if(view.equals(badd))
+             c = "+";
+         else if(view.equals(bsub))
+            c = "-";
+         else if(view.equals(bdivide))
+            c = "/";
+         else if(view.equals(bx))
+            c = "*";
+
+         dis= temp+" "+c;
+         setvalue(dis);
+    }
+
+
+    public void result(View view) {
+        double op1 = Double.parseDouble(current_value);
+        double op2 = Double.parseDouble(temp);
+        if(c == "+"){
+            op2 = op2 + op1;
+            setvalue(op2);
+        }
+        else if(c == "-"){
+            op2 = op2 - op1;
+            setvalue(op2);
+        }
+        else if(c == "*"){
+            op2 = op2 * op1;
+            setvalue(op2);
+        }
+        else if(c == "/"){
+            op2 = op2 / op1;
+            setvalue(op2);
+        }
+        current_value = String.valueOf(op2);
+    }
+
+    public void percentage(View view) {
+        double per = Double.parseDouble(current_value);
+        per/=100;
+        setvalue(per);
+    }
 }
